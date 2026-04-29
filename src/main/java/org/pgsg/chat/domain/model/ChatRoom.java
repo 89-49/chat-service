93762d4d29f13caf5399582c15023876fec82370
kimdh32022 <kimdh32022@naver.com>
2026-04-29 14:57:04 +0900

    채팅 Root Aggregate 구현
    - ChatRoom: 채팅 방을 생성하기 위한 Enum,Vo를 포함한 도메인 및 도메인 로직 구현
    - ChatMessage: 채팅 메세지를  1:N관계로 채팅 방과 관계를 맺음.

---
+++ src/main/java/org/pgsg/chat/domain/model/ChatRoom.java
@@ -0,0 +1,84 @@
+package org.pgsg.chat.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+import org.pgsg.chat.domain.event.ChatEvents;
+import org.pgsg.common.domain.BaseEntity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@ToString
+@Table(name = "p_chat_room")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
+public class ChatRoom extends BaseEntity {
+
+    @EmbeddedId
+    private RoomId id;
+
+    @Embedded
+    private Seller seller;
+
+    @Embedded
+    private Buyer buyer;
+
+    @Embedded
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    private RoomStatus status;
+
+
+    @JoinColumn(name="chatroom_id")
+    @OrderBy("createdAt ASC")
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatMessage> messages = new ArrayList<>();
+
+    @Builder
+    public ChatRoom(UUID tradeId,UUID productId, String productName, UUID sellerId,String sellerNickName,UUID buyerId,String buyerNickName) {
+        this.id = RoomId.of(tradeId);
+        this.seller = new Seller(sellerId, sellerNickName);
+        this.buyer = new Buyer(buyerId, buyerNickName);
+        this.product = new Product(productId, productName);
+        this.status = RoomStatus.TRADING;
+    }
+
+    // 채팅 메세지 등록
+    public void addMessage(SenderType type, String content){
+        this.messages.add(ChatMessage.of(type, content));
+    }
+
+    // 마지막 메세지 등록 일시
+    public LocalDateTime getLastMessageAt() {
+        return this.messages == null || this.messages.isEmpty() ? null : this.messages.getLast().getCreatedAt();
+    }
+
+    // 완료 상태 변경
+    public void complete(ChatEvents events) {
+        if(this.status == RoomStatus.COMPLETED){ // 이미 완료 상태이면 처리 X, 멱등성 처리
+            return;
+        }
+
+        this.status = RoomStatus.COMPLETED;
+
+        // 완료 상태 변경 후 후속 처리 알림
+        events.completed(this);
+    }
+
+    // 취소 상태 변경
+    public void cancel(ChatEvents events) {
+        if(this.status == RoomStatus.CANCELED){ // 이미 취소 상태이면 처리 X, 멱등성 처리
+            return;
+        }
+        this.status = RoomStatus.CANCELED;
+
+        // 취소 상태 변경 후 후속 처리 알림
+        events.canceled(this);
+    }
+ }
