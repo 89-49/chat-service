4f5c5d13513ccacbdc06eeb4ced2d5901f26647d
kimdh32022 <kimdh32022@naver.com>
2026-04-30 23:31:15 +0900

    chore: WebSocketConfig setting

---
+++ src/main/java/org/pgsg/chat/infrastructure/Websocket/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package org.pgsg.chat.infrastructure.Websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트에서 메세지 받을 엔드포인트 prefix
+        registry.setApplicationDestinationPrefixes("/app"); // 발행
+        registry.enableSimpleBroker("/topic"); // 구독
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+}
