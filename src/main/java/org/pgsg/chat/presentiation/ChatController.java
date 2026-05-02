7950bd7925842763110ed6d8c1b0987b1ac18125
kimdh32022 <kimdh32022@naver.com>
2026-05-02 19:25:34 +0900

    feat: 채팅 메세지 전달 API  및 UI 구현 (#15)
    - 채팅 컨트롤러로 endPoint를 통해 해당 룸에 채팅을 보내 소통하는 경로 구현.
    - 테스트를 위해 UI를 간단하게 만들어서 채팅으로 소통 확인함.

---
+++ src/main/java/org/pgsg/chat/presentiation/ChatController.java
@@ -0,0 +1,28 @@
+package org.pgsg.chat.presentiation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.pgsg.chat.application.service.ChatService;
+import org.pgsg.chat.presentiation.dto.ChatRelay;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/{roomId}/message")
+    public void chatRelay(@DestinationVariable("roomId") UUID roomId, ChatRelay chatRelay) {
+        log.info("메세지 전송: roomId: {}, chatRelay: {}", roomId, chatRelay);
+        chatService.addMessage(roomId, chatRelay.type(), chatRelay.message());
+        messagingTemplate.convertAndSend("/topic/room/"+ roomId,chatRelay);
+        }
+    }
