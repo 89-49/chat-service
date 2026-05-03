26ca18faf18fad3419bf32d26cef213fb48e7876
kimdh32022 <kimdh32022@naver.com>
2026-05-03 20:29:33 +0900

    feat: Presentation 계층의 단건 조회 API 코드 구현
    - 프론트엔드에서 roomId를 전달 받아 DB에 해당 룸의 정보를 가져오는 API 라우팅 코드 구현했습니다.

--- src/main/java/org/pgsg/chat/presentiation/ChatController.java
+++ src/main/java/org/pgsg/chat/presentiation/ChatController.java
@@ -2,27 +2,43 @@ package org.pgsg.chat.presentiation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.pgsg.chat.application.dto.RoomInfo;
 import org.pgsg.chat.application.service.ChatService;
+import org.pgsg.chat.application.service.query.ChatQueryService;
 import org.pgsg.chat.presentiation.dto.ChatRelay;
+import org.pgsg.chat.presentiation.dto.FindRoomResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/v1/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
+    private final ChatQueryService chatQueryService;
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/{roomId}/message")
     public void chatRelay(@DestinationVariable("roomId") UUID roomId, ChatRelay chatRelay) {
         log.info("메세지 전송: roomId: {}, chatRelay: {}", roomId, chatRelay);
         chatService.addMessage(roomId, chatRelay.type(), chatRelay.message());
-        messagingTemplate.convertAndSend("/topic/room/"+ roomId,chatRelay);
-        }
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, chatRelay);
     }
+
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<FindRoomResponse> findRoom(@PathVariable("roomId") UUID roomId){
+        FindRoomResponse response = FindRoomResponse.from(chatQueryService.findChatRoom(roomId));
+        return ResponseEntity.ok(response);
+    }
+
+}
