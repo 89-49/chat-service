40bd02db8305d4979a56c7405970bb63d017bdcb
kimdh32022 <kimdh32022@naver.com>
2026-05-04 12:44:44 +0900

    feat: 채팅방 목록 조회 서비스 코드 구현

--- src/main/java/org/pgsg/chat/application/service/query/ChatQueryService.java
+++ src/main/java/org/pgsg/chat/application/service/query/ChatQueryService.java
@@ -1,11 +1,14 @@
 package org.pgsg.chat.application.service.query;
 
 import lombok.RequiredArgsConstructor;
+import org.pgsg.chat.application.dto.FindChatRoomQuery;
 import org.pgsg.chat.application.dto.RoomInfo;
 import org.pgsg.chat.domain.exception.ChatRoomNotFoundException;
 import org.pgsg.chat.domain.model.Room;
 import org.pgsg.chat.domain.model.RoomId;
 import org.pgsg.chat.domain.repository.ChatRoomQueryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,5 +29,10 @@ public class ChatQueryService {
 
         return RoomInfo.from(room);
     }
+
     // 단체 조회
+    public Page<RoomInfo> findChatRooms(FindChatRoomQuery query,  Pageable pageable){
+        return roomQueryRepository.findAll(FindChatRoomQuery.toSearch(query),pageable).map(RoomInfo::from);
+    }
+
 }
